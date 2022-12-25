412ee39453d05a184d459d88425d7a5ff6727efd
mpeilun <peilun2016@gmail.com>
2022-12-25 23:54:45 +0800

    update contact

--- components/home-page/posts.tsx
+++ components/home-page/posts.tsx
@@ -7,7 +7,7 @@ function Post(props: { posts: PostDataType[] }) {
     <section>
       <Box sx={{ p: { md: '1rem 4rem 1rem 4rem', xs: '1rem 2rem 1rem 2rem' } }}>
         <Box sx={{ display: { md: 'block', xs: 'none' } }}>
-          <h2>News</h2>
+          <h2 style={{ textAlign: 'center' }}>News</h2>
         </Box>
 
         <PostsGrid posts={props.posts} />
--- components/posts/all-posts.tsx
+++ components/posts/all-posts.tsx
@@ -6,7 +6,7 @@ function AllPost(props: { posts: PostDataType[] }) {
   return (
     <section>
       <Box sx={{ p: { md: '1rem 4rem 1rem 4rem', xs: '1rem 2rem 1rem 2rem' } }}>
-        <h1>All Posts</h1>
+        <h1 style={{ textAlign: 'center' }}>All Posts</h1>
         <PostsGrid posts={props.posts} />
       </Box>
     </section>
