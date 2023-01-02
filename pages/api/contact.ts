3812f0cdf9032c91561134bdaa7ae5602ce301b8
mpeilun <peilun2016@gmail.com>
2023-01-02 16:43:15 +0800

    add update

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -5,12 +5,10 @@ function handler(req: NextApiRequest, res: NextApiResponse) {
     const { email, name, message } = JSON.parse(req.body) as { email: string; name: string; message: string }
     if (!email || !email.includes('@') || !name || name.trim() === '' || !message || message.trim() === '') {
       res.status(422).json({ message: 'Invalid input.' })
-      return
     }
 
     const validData = { email: email, name: name, message: message }
     res.status(201).json({ message: 'Successfully stored message.', validData: validData })
-    return
   }
 }
 
---
+++ pages/api/test/index.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../prisma/prisma'
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // await prisma.users.deleteMany()
+  // const create = await prisma.users.create({
+  //   data: {
+  //     name: 'peilun',
+  //     googleId: '46679555',
+  //     email: 'peilun2016@gmail.com',
+  //     socialAccount: { github: '#peilun_', ig: 'peter' },
+  //     userpreference: {
+  //       create: {
+  //         emailUpdates: true,
+  //       },
+  //     },
+  //   },
+  //   include: {
+  //     userpreference: true,
+  //   },
+  // })
+
+  const find = await prisma.users.update({
+    where: { email: 'peilun1128@gmail.com' },
+    data: {
+      userpreference: {
+        update: {
+          emailUpdates: true,
+        },
+      },
+    },
+  })
+  res.status(200).json(find)
+
+  // const createMany = await prisma.users.createMany({
+  //   data: {
+  //     name: 'peilun',
+  //     googleId: `${Math.floor(2016 * Math.random())}`,
+  //     email: `peilun${Math.floor(2016 * Math.random())}@gmail.com`,
+  //     socialAccount: { github: '#peilun_', ig: 'peter' },
+  //   },
+  // })
+
+  // res.status(200).json(createMany)
+}
+
+export default handler
