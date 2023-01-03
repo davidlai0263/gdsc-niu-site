5d46421b7e953795b4c68c3508a03eaca8a698e1
Peilun <peilun2016@gmail.com>
2023-01-03 23:08:24 +0800

    add contact rate limit

--- pages/api/test/index.ts
+++ pages/api/test/index.ts
@@ -20,6 +20,17 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   //   },
   // })
 
+  // const createMany = await prisma.users.createMany({
+  //   data: {
+  //     name: 'peilun',
+  //     googleId: `${Math.floor(2016 * Math.random())}`,
+  //     email: `peilun${Math.floor(2016 * Math.random())}@gmail.com`,
+  //     socialAccount: { github: '#peilun_', ig: 'peter' },
+  //   },
+  // })
+
+  // res.status(200).json(createMany)
+
   const find = await prisma.users.update({
     where: { email: 'peilun1128@gmail.com' },
     data: {
@@ -31,17 +42,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     },
   })
   res.status(200).json(find)
-
-  // const createMany = await prisma.users.createMany({
-  //   data: {
-  //     name: 'peilun',
-  //     googleId: `${Math.floor(2016 * Math.random())}`,
-  //     email: `peilun${Math.floor(2016 * Math.random())}@gmail.com`,
-  //     socialAccount: { github: '#peilun_', ig: 'peter' },
-  //   },
-  // })
-
-  // res.status(200).json(createMany)
 }
 
 export default handler
